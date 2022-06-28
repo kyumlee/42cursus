ba5e1a5b16ee7628aa18cc23ba4f4266c9983d93
kyumlee <kyumlee@student.42seoul.kr>
2022-06-28 11:15:12 +0900

    refactor server.cpp and location.cpp

--- circle-05/webserv/includes/Location.hpp
+++ circle-05/webserv/includes/Location.hpp
@@ -4,35 +4,54 @@
 # include "./Utils.hpp"
 
 /*
- * location optional_modifier location_match {
+ * location optional_modifier location_route {
  * ...
  * }
  *
  * optional_modifier: (none), =, ~, ~*, ^~.
- * location_match: what Nginx should check the request URI against.
+ * location_route: what Nginx should check the request URI against.
  */
 
 class	Location {
 
 	private:
-		std::string	_mod;	// optional modifier
-		std::string	_match;	// location match
+		std::string					_block;
+		std::string					_mod;		// optional modifier
+		std::string					_route;		// location match
+		std::vector<std::string>	_methods;	// GET, POST, DELETE
+		int							_redirect;	// if a response code is 302, inform to re-request to another [Location]
+		bool						_dListing;	// autoindex on/off
+		std::string					_default;	// if a request is a directory, return this file
 
 	public:
 		// constructor, destructor, assignment operator overload
 		Location ();
-		Location (std::string mod, std::string match);
 		Location (const Location &lb);
 		~Location ();
 		Location	&operator= (const Location &lb);
 
 		// getter
-		std::string	getMod ();
-		std::string	getMatch ();
+		std::string					getBlock ();
+		std::string					getMod ();
+		std::string					getRoute ();
+		std::vector<std::string>	getMethods ();
+		int							getRedirect ();
+		bool						getDListing ();
+		std::string					getDefault ();
 
 		// setter
-		void		setMod (std::string str);
-		void		setMatch (std::string str);
+		void						setBlock (std::string block);
+		void						setMod (std::string mod);
+		void						setRoute (std::string route);
+		void						setMethods (std::vector<std::string> methods);
+		void						setRedirect (int redirection);
+		void						setDListing (bool dListing);
+		void						setDefault (std::string file);
+
+		int							parseMethods ();
+		int							parseDListing ();
+		int							parseDefault ();
+		void						parse ();
 };
 
 #endif
