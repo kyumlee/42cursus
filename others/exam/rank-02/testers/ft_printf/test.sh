360019124b505238f7f1db58b91ed5395ebff22a
kyumlee <kyumlee@student.42seoul.kr>
2022-11-14 20:05:48 +0900

    deleted finished exams

--- others/exam/rank-02/testers/ft_printf/test.sh
+++
@@ -1,9 +0,0 @@
-cd ../../my_code/ft_printf/
-make
-git clone https://github.com/gavinfielder/pft.git pft_exam && echo "pft_exam/" >> .gitignore && cd pft_exam && rm unit_tests.c && rm options-config.ini && git clone https://github.com/cclaude42/PFT_EXAM.git temp && cp temp/unit_tests.c . && cp temp/options-config.ini . && rm -rf temp
-cd pft_exam/
-make
-./test
-cd ../
-make fclean
-rm -rf pft_exam
